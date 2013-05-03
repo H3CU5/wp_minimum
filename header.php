612ac9de6ad43f90aa165469ba7c18262d1ed189
Hecus <partner@palatia-pop.de>
2013-05-03 12:09:39 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<?php
+///////////////// This is the Header //////////////////////
+?>
+<html><!-- tag im Footer schliessen! -->
+    <head>
+
+    </head>
+  <body> <!-- tag im Footer schliessen! -->
